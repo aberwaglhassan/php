99b8a3415fec1197e0a92b3762cc9efe932ee608
unknown <laberwag@gmail.com>
2018-02-06 14:28:29 +0100

    decoupage en vues

--- show.php
+++ show.php
@@ -1,36 +1,19 @@
 <?php
-// index.php
+
 $link = new PDO("mysql:host=localhost;dbname=blog_db;charset=utf8",'blog_db', '');
 
 
 
-?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>
-            exophp
-        </title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    <h1>Liste des articles</h1>
 
-        <?php
 
         $query = 'SELECT title, description FROM articles WHERE  id=:id';
         $statement=$link->prepare($query);
         $statement->bindValue(':id',$_GET['id'],PDO::PARAM_INT);
         $statement->execute();
 
-        $row=$statement->fetch(PDO::FETCH_ASSOC);?>
-
-                 <?=$row['description']?>
+        $row=$statement->fetch(PDO::FETCH_ASSOC);
 
 
 
-    </body>
-    </html>
-<?php
-$link = null;
-?>
+
+require 'templates/detail.php';
---
+++ templates/detail.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>
+details articles
+        </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>Liste des articles</h1>
+
+
+
+                 <?= $row['description'] ?>
+
+
+
+</body>
+</html>

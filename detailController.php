f969dd2e79acc4d8f8233042d779ce377a6baaf8
unknown <laberwag@gmail.com>
2018-02-07 11:15:03 +0100

    ajout des restriction

--- detailController.php
+++ detailController.php
@@ -1,10 +1,18 @@
 <?php
-
-require_once 'model/model.php';
-
-        $row= get_description();
-
-
-
-
-require 'templates/show.php';
+require_once ('model/model.php');
+if(!is_numeric($_GET['id']))
+{
+    header('Location: HTTP/1.1 404 not found');
+    die();
+}
+if(isset($_GET['id'])) {
+    $row = get_description();
+    if(!$row){
+        echo '<h2 class="error">Cet article n\'existe pas</h2>';
+        die();
+    }
+}else{
+    header('Location: HTTP/1.1 404 not found');
+}
+require 'templates/view/show.php';
+?>
